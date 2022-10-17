249be2a6547afb59571c16b8538e11145a475c3f
Divyank <19bt04047@gsfcuniversity.ac.in>
2022-10-17 14:35:54 +0530

    Initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return(
+        <div class="nav">
+            <a href="#">Logo</a>
+            <div class="wrap">
+                <a href="upload">Upload Song</a>
+            <a href="login">Login</a>
+            <a href="Signup">Signup</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
